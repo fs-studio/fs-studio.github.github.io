02e070fa0f5a6ba045b55fa42135b73f07f55edb
melkumov-11 <melkumov-11@yandex.ru>
2019-08-18 00:33:10 +0300

    Commit

--- boat_trip/js/script.js
+++ boat_trip/js/script.js
@@ -1,6 +1,10 @@
 $(function() {
-  $('.header-top__left_hamburger').click(function() {
-    $(this).toggleClass('hamburger_active');
-    $('.header-top__left nav').toggle(300);
+  $('#more').click(function(e) {
+    e.preventDefault;
+//    if($('.assortment-block_hidden').css('display') === 'none') {
+//      $('.assortment-block_hidden').css('display', 'block');
+//    }
+    $('.assortment-block_hidden').toggle(300);
+    $(this).text('Скрыть');
   });
 });
