4537fb88e9f92fdd85b99789e0905c69b639c5b0
melkumov-11 <37487258+melkumov-11@users.noreply.github.com>
2019-08-23 17:43:18 +0300

    Большой коммит

--- boat_trip/js/script.js
+++ boat_trip/js/script.js
@@ -7,4 +7,12 @@ $(function() {
     $('.assortment-block_hidden').toggle(300);
     $(this).css('display', 'none');
   });
+  
+  $('#modal-close').click(function() {
+    $('.modal').hide(300);
+  });
+  $('#anapa').click(function() {
+    $('#anapa-modal').show(300);
+    $('.slick-track').css('width', '808px');
+  })
 });
--- boat_trip/js/slider.js
+++ boat_trip/js/slider.js
@@ -1,14 +1,34 @@
+//$('.slider-for').slick({
+//  slidesToShow: 1,
+//  slidesToScroll: 1,
+//  arrows: false,
+//  fade: true,
+//  asNavFor: '.slider-nav'
+//});
+//$('.slider-nav').slick({
+//  slidesToShow: 3,
+//  slidesToScroll: 1,
+//  asNavFor: '.slider-for',
+//  centerMode: true,
+//  focusOnSelect: true
+//});
 $('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  speed: 2500,
+  infinite: true,
+  dots: true,
   arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
+  fade: true
 });
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  centerMode: true,
-  focusOnSelect: true
-});
+
+//$('.slider-nav').slick({
+//  slidesToShow: 4,
+//  autoplay: true,
+//  autoplaySpeed: 5000,
+//  infinite: true,
+//  asNavFor: '.slider-for',
+//  arrows: false,
+//  draggable: false,
+//  focusOnSelect: true
+//});
