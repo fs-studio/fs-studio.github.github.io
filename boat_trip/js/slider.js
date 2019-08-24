ea78dc602c25bd3b81c1eec12bc300960f72d75d
melkumov-11 <melkumov-11@yandex.ru>
2019-08-24 15:35:47 +0300

    Завершение

--- boat_trip/js/slider.js
+++ boat_trip/js/slider.js
@@ -1,17 +1,3 @@
-//$('.slider-for').slick({
-//  slidesToShow: 1,
-//  slidesToScroll: 1,
-//  arrows: false,
-//  fade: true,
-//  asNavFor: '.slider-nav'
-//});
-//$('.slider-nav').slick({
-//  slidesToShow: 3,
-//  slidesToScroll: 1,
-//  asNavFor: '.slider-for',
-//  centerMode: true,
-//  focusOnSelect: true
-//});
 $('.slider').slick({
   autoplay: true,
   autoplaySpeed: 5000,
@@ -21,14 +7,3 @@ $('.slider').slick({
   arrows: false,
   fade: true
 });
-
-//$('.slider-nav').slick({
-//  slidesToShow: 4,
-//  autoplay: true,
-//  autoplaySpeed: 5000,
-//  infinite: true,
-//  asNavFor: '.slider-for',
-//  arrows: false,
-//  draggable: false,
-//  focusOnSelect: true
-//});
