969b8a1c26ce461709471713528de7ba8bc423e9
fs-studio <53426834+fs-studio@users.noreply.github.com>
2019-08-26 14:57:18 +0300

    Preloader
    A preloader was added on pages

---
+++ cis/js/script.js
@@ -0,0 +1,16 @@
+/* Preloader */
+
+$(function() {
+  $(window).on('load', function() {
+    setTimeout(function() {
+      var fspagepreloader = $(".preloader");
+      if (!fspagepreloader.hasClass("preloader_disable")) {
+        fspagepreloader.removeClass("preloader_active");
+        fspagepreloader.addClass("preloader_disable");
+      }
+    }, 1500);
+  });
+});
+
+
+/* Preloader */
