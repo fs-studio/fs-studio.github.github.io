5ae79f927167fab669255a50f6f29c1318a02818
fs-studio <53426834+fs-studio@users.noreply.github.com>
2019-10-18 17:32:48 +0300

    Preland

---
+++ prelands/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="robots" content="index, follow" />
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <title>Акция! Получи сайт бесплатно!</title>
+
+  <link rel="preload" href="css/styles.css" as="style" />
+</head>
+<body>
+
+  <div class="container">
+    <div class="container__logo">
+      <div class="logo">
+        <img src="images/logo.png" class="image image__logo" />
+        <h1 class="caption caption_h1 caption__logo">DigitalCreations</h1>
+      </div>
+    </div>
+    <div class="container__content">
+      <div class="caption-block">
+        <h1 class="caption caption_h1 caption__caption-block">Закажи разработку сайта</h1>
+        <p class="text text__caption">
+          Our guests from developers are coming to ArtCenter hall to talk about nowadays digital novelties and plans for the future projects.
+          Hurry up to sign up!
+        </p>
+      </div>
+      <div class="blocks">
+        <div class="blocks__element">
+          <div class="element-image">
+            <img src="" class="image image__element" />
+          </div>
+          <div class="element-text">
+            <p class="text text__element">
+              Профессиональная разработка
+            </p>
+          </div>
+        </div>
+        <div class="blocks__element">
+          <div class="element-image">
+            <img src="" class="image image__element" />
+          </div>
+          <div class="element-text">
+            <p class="text text__element">
+              Быстро и качественно
+            </p>
+          </div>
+        </div>
+        <div class="blocks__element">
+          <div class="element-image">
+            <img src="" class="image image__element" />
+          </div>
+          <div class="element-text">
+            <p class="text text__element">
+              Бесплатно
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container__button">
+      <p class="text text__button">
+        Только сегодня! Оставь заявку и получи два варианта дизайна в подарок!
+      </p>
+      <a href="javascript:void(0);" class="link button link__button">Получить сейчас</a>
+    </div>
+  </div>
+
+  <link rel="stylesheet" href="css/styles.css" />
+</body>
+</html>
