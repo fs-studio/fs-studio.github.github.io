4ab1d58d19caa5c51d5a5a4f2bf39c53367b65dc
fs-studio <53426834+fs-studio@users.noreply.github.com>
2019-10-22 18:12:59 +0300

    Кнопка

--- preland/js/scripts.js
+++ preland/js/scripts.js
@@ -3,12 +3,12 @@
 $(".button_ripple").on("click", function(e) {
     $(this).append("<span class=\"ripple-effect\">");
     $(this).find(".ripple-effect").css({
-        left: e.pageX - $(this).position().left,
-        top: e.pageY - $(this).position().top
+        left: e.pageX - $(this).offset().left,
+        top: e.pageY - $(this).offset().top
     }).animate({
         opacity: 0
     }, 1500, function() {
-        $(this).remove();
+        //$(this).remove();
     });
 });
 
