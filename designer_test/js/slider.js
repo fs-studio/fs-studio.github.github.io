88caccea521c0639aa6bfc9c91f8db8d9f3037ab
fs-studio <53426834+fs-studio@users.noreply.github.com>
2019-11-28 23:35:32 +0300

    Коммит

---
+++ designer_test/js/slider.js
@@ -0,0 +1,498 @@
+$(function() {
+  
+  $("#bg-slider1").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'bounceOut',
+  	animateIn: 'bounceIn',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider2").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'bounceOutDown',
+  	animateIn: 'bounceInDown',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider3").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'bounceOutLeft',
+  	animateIn: 'bounceInLeft',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider4").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'bounceOutRight',
+  	animateIn: 'bounceInRight',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider5").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'bounceOutUp',
+  	animateIn: 'bounceInUp',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider6").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOut',
+  	animateIn: 'fadeIn',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider7").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOutDown',
+  	animateIn: 'fadeInDown',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider8").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOutDownBig',
+  	animateIn: 'fadeInDownBig',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider9").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOutLeft',
+  	animateIn: 'fadeInLeft',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider10").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOutLeftBig',
+  	animateIn: 'fadeInLeftBig',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider11").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOutRight',
+  	animateIn: 'fadeInRight',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider12").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOutRightBig',
+  	animateIn: 'fadeInRightBig',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider13").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOutUp',
+  	animateIn: 'fadeInUp',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider14").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'fadeOutUpBig',
+  	animateIn: 'fadeInUpBig',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider15").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'flip',
+  	animateIn: 'flip',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider16").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'flipOutX',
+  	animateIn: 'flipInX',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider17").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'flipOutY',
+  	animateIn: 'flipInY',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider18").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'lightSpeedOut',
+  	animateIn: 'lightSpeedIn',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider19").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'rotateOut',
+  	animateIn: 'rotateIn',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider20").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'rotateOutDownLeft',
+  	animateIn: 'rotateInDownLeft',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider21").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'rotateOutDownRight',
+  	animateIn: 'rotateInDownRight',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider22").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'rotateOutUpLeft',
+  	animateIn: 'rotateInUpLeft',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider23").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'rotateOutUpRight',
+  	animateIn: 'rotateInUpRight',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider24").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'slideOutUp',
+  	animateIn: 'slideInUp',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider25").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'slideOutDown',
+  	animateIn: 'slideInDown',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider26").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'slideOutLeft',
+  	animateIn: 'slideInLeft',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider27").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'slideOutRight',
+  	animateIn: 'slideInRight',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider28").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'zoomOut',
+  	animateIn: 'zoomIn',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider29").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'zoomOutDown',
+  	animateIn: 'zoomInDown',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider30").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'zoomOutLeft',
+  	animateIn: 'zoomInLeft',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider31").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'zoomOutRight',
+  	animateIn: 'zoomInRight',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider32").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'zoomOutUp',
+  	animateIn: 'zoomInUp',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider33").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'hinge',
+  	// animateIn: 'hinge',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider34").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	// animateOut: 'jackInTheBox',
+  	animateIn: 'jackInTheBox',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider35").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'rollOut',
+  	animateIn: 'rollIn',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider36").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'bounce',
+  	animateIn: 'bounce',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider37").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'flash',
+  	animateIn: 'flash',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider38").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'pulse',
+  	animateIn: 'pulse',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider39").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'rubberBand',
+  	animateIn: 'rubberBand',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider40").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'shake',
+  	animateIn: 'shake',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider41").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'swing',
+  	animateIn: 'swing',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider42").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'tada',
+  	animateIn: 'tada',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider43").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'wobble',
+  	animateIn: 'wobble',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider44").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'jello',
+  	animateIn: 'jello',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+  $("#bg-slider45").owlCarousel({
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	animateOut: 'heartBeat',
+  	animateIn: 'heartBeat',
+  	dots: false,
+  	smartSpeed: 2500
+  });
+
+});
