fbc17fb3be0641a4a87e6aba5c3abbb8c528a012
fs-studio <53426834+fs-studio@users.noreply.github.com>
2019-12-22 23:58:26 +0300

    +

--- doma_naveka/js/slider.js
+++ doma_naveka/js/slider.js
@@ -1,3 +1,15 @@
-$('.tab-content').owlCarousel({
-  
+let tabContentActive = $('.tab-content__active').owlCarousel({
+    margin: 30,
+    dotsContainer: '.nav-dots',
+    dotClass: 'nav-dots__elem',
+    loop: true,
+    smartSpeed: 1000
+});
+
+$('.nav-prev').click(function() {
+    tabContentActive.trigger('prev.owl.carousel');
+});
+
+$('.nav-next').click(function() {
+    tabContentActive.trigger('next.owl.carousel');
 });
